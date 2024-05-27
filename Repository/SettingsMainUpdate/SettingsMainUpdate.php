54e3d3d60ab35905be57b95c875e2df358630695
kingstar <admin@baks.dev>
2024-05-27 21:42:13 +0300

    Baks Development

---
+++ Repository/SettingsMainUpdate/SettingsMainUpdate.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace BaksDev\Settings\Main\Repository\SettingsMainUpdate;
+
+
+use BaksDev\Core\Doctrine\ORMQueryBuilder;
+use BaksDev\Settings\Main\Entity\Event\SettingsMainEvent;
+use BaksDev\Settings\Main\Entity\SettingsMain;
+use BaksDev\Settings\Main\Type\Id\SettingsMainIdentificator;
+
+final class SettingsMainUpdate implements SettingsMainUpdateInterface
+{
+
+    //private EntityManagerInterface $entityManager;
+
+    private SettingsMainIdentificator $id;
+    private ORMQueryBuilder $ORMQueryBuilder;
+
+
+    public function __construct(ORMQueryBuilder $ORMQueryBuilder)
+    {
+        $this->id = new SettingsMainIdentificator();
+        $this->ORMQueryBuilder = $ORMQueryBuilder;
+    }
+
+
+    public function get(): ?SettingsMainEvent
+    {
+        $qb = $this->ORMQueryBuilder->createQueryBuilder(self::class);
+
+        $qb
+            ->from(SettingsMain::class, 'settings')
+            ->where('settings.id = :settings')
+            ->setParameter(':settings', $this->id, SettingsMainIdentificator::TYPE);
+
+        $qb
+            ->select('event')
+            ->join(
+                SettingsMainEvent::class,
+                'event',
+                'WITH',
+                'event.id = settings.event'
+            );
+
+
+        return $qb->getOneOrNullResult();
+
+    }
+
+}
