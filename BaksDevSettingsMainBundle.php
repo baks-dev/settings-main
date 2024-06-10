c994f4a4ba33a469d1a3b8760cabda79485d1444
kingstar <admin@baks.dev>
2024-06-10 10:40:21 +0300

    Baks Development

--- BaksDevSettingsMainBundle.php
+++ BaksDevSettingsMainBundle.php
@@ -12,6 +12,8 @@ declare(strict_types=1);
 
 namespace BaksDev\Settings\Main;
 
+use BaksDev\Core\Repository\SettingsMain\SettingsMainInterface;
+use BaksDev\Settings\Main\Repository\SettingsMain\SettingsMainRepository;
 use DirectoryIterator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -23,5 +25,22 @@ class BaksDevSettingsMainBundle extends AbstractBundle
     public const NAMESPACE = __NAMESPACE__.'\\';
 
     public const PATH = __DIR__.DIRECTORY_SEPARATOR;
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $services = $container->services()
+            ->defaults()
+            ->autowire()
+            ->autoconfigure();
+
+        $services->load(self::NAMESPACE, self::PATH)
+            ->exclude([
+                self::PATH.'{Entity,Resources,Type}',
+                self::PATH.'**/*Message.php',
+                self::PATH.'**/*DTO.php',
+            ]);
+
+        $services->alias(SettingsMainInterface::class.' $settingsMain', SettingsMainRepository::class);
+    }
 	
 }
